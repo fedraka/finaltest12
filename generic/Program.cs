8613cff52598459e03794ebd6e48b6392f633f1a
FKarshenas <FKarshenas@RP-HARTS05>
2021-01-18 20:37:41 -0500

    second

--- generic/Program.cs
+++ generic/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+
+
 namespace generic
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            List<int> myList = new List<int> { 1,2, 3 };
-        }
-    }
+       public static void Main()
+		{
+            var prs = new PersonDoc<Person>();
+
+
+            prs.AddPerson(new Person("Shahab", "Tima"));
+            prs.AddPerson(new Person("Sheri", "Tima"));
+
+            prs.ReturnPerson(1);
+
+			prs.AllReturnPerson();
+			
+		}
+
+
+		public class PersonDoc<TPerson>where TPerson : IPerson 
+		{
+			private readonly List<TPerson> PersonList = new List<TPerson>();
+
+			public void AddPerson(TPerson person)
+			{
+					PersonList.Add(person);
+			}
+
+			public void ReturnPerson(int num )
+			{
+				Console.WriteLine(PersonList[num].Firstname +" " + PersonList[num].Lastname);
+			}
+
+			public void AllReturnPerson()
+			{
+			//	var newList =from s in PersonList orderby s.Person.Firstname
+					//PersonList.order(x => x.fi).ToList();
+
+				foreach (TPerson p in PersonList)
+                {
+                    Console.WriteLine(p.Firstname);
+                }
+            }
+
+		}
+
+
+
+		public interface IPerson
+		{
+			string Firstname {get;set;}
+
+			string Lastname{ get; set; }
+		}
+
+		public class Person : IPerson
+		{
+			public Person()
+			{
+			}
+			public Person(string first, string last)
+			{
+				this.Firstname = first;
+				this.Lastname = last;
+			}
+
+			public string Firstname { get; set; }
+
+			public string Lastname { get; set; }
+		}
+
+	}
 }
