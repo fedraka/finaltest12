0aa5c151372fbcee1164577a6debee79dd2353f4
FKarshenas <FKarshenas@RP-HARTS05>
2021-01-18 19:59:02 -0500

    first commit

---
+++ generic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int> { 1,2, 3 };
+        }
+    }
+}
